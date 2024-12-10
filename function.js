92a1de758d4b70f168f856f144080ce9c4d78564
khaigrizz <razer12388@gmail.com>
2024-12-10 16:26:09 +0800

    Update Buttons

--- function.js
+++ function.js
@@ -1,2 +1,6 @@
-function NextPage(){
+function Surprise(){
     window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";}
+
+function Skibidi(){
+    window.location.href = "https://www.youtube.com/watch?v=TxanlC8ZnTY";
+}
